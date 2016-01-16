e38af6e616067a845a0d7245f7b1b8ae3110dd05
Michael P. Gerlek <mpg@flaxen.com>
2016-01-15 20:29:23 -0500

    POST and GET and DELETE

--- alerter.go
+++ alerter.go
@@ -3,26 +3,60 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"github.com/gin-gonic/gin"
+	"github.com/venicegeo/pz-gocommon"
+	"log"
+	"net/http"
 	"os"
 	"strings"
-	"github.com/venicegeo/pz-gocommon"
-	"net/http"
+	"strconv"
 )
 
-var database map[int]Alert
-var id int
+var alertId int = 1
 
 type Alert struct {
-	id int
-	Name string `json:"name" binding:"required"`
+	Id        string `json:"id"`
+	Name      string `json:"name" binding:"required"`
 	Condition string `json:"condition" binding:"required"`
+}
+
+type AlertDB struct {
+	data map[string]Alert
+}
+
+func newAlertDB() *AlertDB {
+	db := new(AlertDB)
+	db.data = make(map[string]Alert)
+	return db
+}
+
+func (db *AlertDB) write(alert* Alert) error {
+	alert.Id = strconv.Itoa(alertId)
+	db.data[alert.Id] = *alert
+	alertId++
+	return nil
+}
+
+func (db *AlertDB) readById(id string) *Alert {
+	v, ok := db.data[id]
+	if !ok {
+		return nil
+	}
+	return &v
+}
+
+func (db *AlertDB) deleteById(id string) bool {
+	_, ok := db.data[id]
+	if !ok {
+		return false
+	}
+	delete(db.data, id)
+	return true
 }
 
 func runAlertServer(discoveryURL string, port string) error {
 
-	database = make(map[int]Alert)
+	db := newAlertDB()
 
 	myAddress := fmt.Sprintf("%s:%s", "localhost", port)
 	myURL := fmt.Sprintf("http://%s/alerts", myAddress)
@@ -33,32 +67,53 @@ func runAlertServer(discoveryURL string, port string) error {
 		return err
 	}
 
-	// TODO
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
 
 	router.POST("/alerts", func(c *gin.Context) {
-		var js Alert
-		err := c.BindJSON(&js)
+		var alert Alert
+		err := c.BindJSON(&alert)
 		if err != nil {
 			c.Error(err)
 			return
 		}
-		js.id = id
-		database[id] = js
-		id++
-
-		c.JSON(http.StatusCreated, gin.H{"id": fmt.Sprintf("%d",id)})
-		//		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-
-
+		err = db.write(&alert)
+		if err != nil {
+			c.Error(err)
+			return
+		}
+		c.JSON(http.StatusCreated, gin.H{"id": alert.Id})
 	})
 
-	// Listen and server on 0.0.0.0:8080
+	router.GET("/alerts", func(c *gin.Context) {
+		c.JSON(http.StatusOK, db.data)
+	})
+
+	router.GET("/alerts/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		v := db.readById(id)
+		if v == nil {
+			c.JSON(http.StatusNotFound, gin.H{"id": id})
+			return
+		}
+		c.JSON(http.StatusOK, v)
+	})
+
+	router.DELETE("/alerts/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		ok := db.deleteById(id)
+		if !ok {
+			c.JSON(http.StatusNotFound, gin.H{"id": id})
+			return
+		}
+		c.JSON(http.StatusOK, nil)
+	})
+
 	err = router.Run("localhost:" + port)
 	return err
 }
+
 
 func app() int {
 	var discoveryURL = flag.String("discovery", "http://localhost:3000", "URL of pz-discovery")
--- alerter_test.go
+++ alerter_test.go
@@ -3,14 +3,15 @@ package main
 import (
 	"encoding/json"
 	//"io/ioutil"
-	"net/http"
-	"testing"
-	"time"
 	"fmt"
 	assert "github.com/stretchr/testify/assert"
 	piazza "github.com/venicegeo/pz-gocommon"
+	"net/http"
+	"testing"
+	"time"
 	//pztesting "github.com/venicegeo/pz-gocommon/testing"
 	"bytes"
+	"io/ioutil"
 )
 
 // @TODO: need to automate call to setup() and/or kill thread after each test
@@ -34,6 +35,49 @@ func TestOkay(t *testing.T) {
 
 	resp, err := http.Post("http://localhost:12342/alerts", piazza.ContentTypeJSON, body)
 	assert.NoError(t, err)
-
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	var s []byte
+	s, _ = ioutil.ReadAll(resp.Body)
+	t.Log(string(s))
+
+	body = bytes.NewBuffer(data)
+	resp, err = http.Post("http://localhost:12342/alerts", piazza.ContentTypeJSON, body)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	s, _ = ioutil.ReadAll(resp.Body)
+	t.Log(string(s))
+
+	resp, err = http.Get("http://localhost:12342/alerts")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	s, _ = ioutil.ReadAll(resp.Body)
+	t.Log(string(s))
+
+	resp, err = http.Get("http://localhost:12342/alerts/1")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	s, _ = ioutil.ReadAll(resp.Body)
+	t.Log(string(s))
+
+	resp, err = piazza.Delete("http://localhost:12342/alerts/1")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	resp, err = http.Get("http://localhost:12342/alerts/1")
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+
+	resp, err = piazza.Delete("http://localhost:12342/alerts/2")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	resp, err = http.Get("http://localhost:12342/alerts")
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	s, _ = ioutil.ReadAll(resp.Body)
+	e := make([]byte, 2)
+	e[0] = 0x7b ; e[1] = 0x7d
+	assert.Equal(t, e, s[:2])
+	t.Log(s)
 }
