fc2a021d9b40e0cc8a197a5813136b9939641d75
Michael P. Gerlek <mpg@flaxen.com>
2016-02-03 08:32:31 -0500

    now using alerter interface for unit tests

--- alerter_test.go
+++ alerter_test.go
@@ -14,6 +14,7 @@ import (
 
 type AlerterTester struct {
 	suite.Suite
+	client *PzAlerterClient
 }
 
 func (suite *AlerterTester) SetupSuite() {
@@ -27,6 +28,8 @@ func (suite *AlerterTester) SetupSuite() {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	suite.client = NewPzAlerterClient("localhost:12342")
 }
 
 func (suite *AlerterTester) TearDownSuite() {
@@ -38,154 +41,24 @@ func TestRunSuite(t *testing.T) {
 	suite.Run(t, s)
 }
 
-
-//---------------------------------------------------------------------------
-
-func postEvent(t *testing.T, event piazza.Event) string {
-
-	data, err := json.Marshal(event)
-	assert.NoError(t, err)
-
-	body := bytes.NewBuffer(data)
-
-	resp, err := http.Post("http://localhost:12342/v1/events", piazza.ContentTypeJSON, body)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-
-	d, err := ioutil.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	defer resp.Body.Close()
-
-	var x map[string]string
-	err = json.Unmarshal(d, &x)
-	assert.NoError(t, err)
-	assert.Contains(t, x, "ID")
-
-	return x["ID"]
-}
-
-func getEvents(t *testing.T) []string {
-	resp, err := http.Get("http://localhost:12342/v1/events")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	d, err := ioutil.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	defer resp.Body.Close()
-
-	var x map[string]interface{}
-	err = json.Unmarshal(d, &x)
-	assert.NoError(t, err)
-
-	var a []string
-	for k := range x {
-		a = append(a, k)
-	}
-	return a
-}
-
-//---------------------------------------------------------------------------
-
-func postCondition(t *testing.T, cond piazza.Condition) string {
-	data, err := json.Marshal(cond)
-	assert.NoError(t, err)
-
-	body := bytes.NewBuffer(data)
-
-	resp, err := http.Post("http://localhost:12342/v1/conditions", piazza.ContentTypeJSON, body)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-
-	d, err := ioutil.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	defer resp.Body.Close()
-
-	var x piazza.AlerterIdResponse
-	err = json.Unmarshal(d, &x)
-	assert.NoError(t, err)
-
-	return x.ID
-}
-
-func getCondition(t *testing.T, id string) bool {
-	resp, err := http.Get("http://localhost:12342/v1/conditions/" + id)
-	assert.NoError(t, err)
-
-	if resp.StatusCode == http.StatusNotFound {
-		return false
-	}
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	d, err := ioutil.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	defer resp.Body.Close()
-
-	var x piazza.Condition
-	err = json.Unmarshal(d, &x)
-	assert.NoError(t, err)
-
-	assert.Equal(t, id, x.ID)
-
-	return true
-}
-
-func getConditions(t *testing.T) []string {
-	resp, err := http.Get("http://localhost:12342/v1/conditions")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	d, err := ioutil.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	defer resp.Body.Close()
-
-	var x map[string]interface{}
-	err = json.Unmarshal(d, &x)
-	assert.NoError(t, err)
-
-	var a []string
-	for k := range x {
-		a = append(a, k)
-	}
-	return a
-}
-
-func deleteCondition(t *testing.T, id string) {
-	resp, err := piazza.Delete("http://localhost:12342/v1/conditions/" + id)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-}
-
-func getAlerts(t *testing.T) []piazza.Alert {
-	resp, err := http.Get("http://localhost:12342/v1/alerts")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	d, err := ioutil.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	defer resp.Body.Close()
-
-	var x map[string]piazza.Alert
-	err = json.Unmarshal(d, &x)
-	assert.NoError(t, err)
-
-	var a []piazza.Alert
-	for i := range x {
-		a = append(a, x[i])
-	}
-	return a
-}
-
 //---------------------------------------------------------------------------
 
 func (suite *AlerterTester) TestConditions() {
 	t := suite.T()
+
+	client := suite.client
+
+	var err error
+	var idResponse *piazza.AlerterIdResponse
 
 	var c1 piazza.Condition
 	c1.Title = "c1"
 	c1.Type = "=type="
 	c1.UserID = "=userid="
 	c1.Date = "=date="
-	c1ID := postCondition(t, c1)
+	idResponse, err = client.PostToConditions(&c1)
+	assert.NoError(t, err)
+	c1ID := idResponse.ID
 	assert.Equal(t, c1ID, "1")
 
 	var c2 piazza.Condition
@@ -193,56 +66,98 @@ func (suite *AlerterTester) TestConditions() {
 	c2.Type = "=type="
 	c2.UserID = "=userid="
 	c2.Date = "=date="
-	c2ID := postCondition(t, c2)
+	idResponse, err = client.PostToConditions(&c2)
+	assert.NoError(t, err)
+	c2ID := idResponse.ID
 	assert.Equal(t, c2ID, "2")
 
-	cs := getConditions(t)
-	assert.Len(t, cs, 2)
-	assert.Contains(t, cs, "1")
-	assert.Contains(t, cs, "2")
+	cs, err := client.GetFromConditions()
+	assert.NoError(t, err)
+	assert.Len(t, *cs, 2)
+	ok1 := false
+	ok2 := false
+	for k, _ := range *cs {
+		if k == "1" {
+			ok1 = true
+		}
+		if k == "2" {
+			ok2 = true
+		}
+	}
+	assert.True(t, ok1 && ok2)
 
-	ok := getCondition(t, "1")
-	assert.True(t, ok)
+	cond, err := client.GetFromCondition("1")
+	assert.NoError(t, err)
+	assert.NotNil(t, cond)
 
-	deleteCondition(t, "1")
+	err = client.DeleteOfCondition("1")
+	assert.NoError(t, err)
 
-	ok = getCondition(t, "1")
-	assert.True(t, !ok)
+	cond, err = client.GetFromCondition("1")
+	assert.Error(t, err) // TODO: should be more refined error here
+	assert.Nil(t, cond)
 
-	deleteCondition(t, "2")
+	err = client.DeleteOfCondition("2")
+	assert.NoError(t, err)
 
-	cs = getConditions(t)
-	assert.Len(t, cs, 0)
+	cs, err = client.GetFromConditions()
+	assert.NoError(t, err)
+	assert.Len(t, *cs, 0)
 }
 
 func (suite *AlerterTester) TestEvents() {
 	t := suite.T()
 
+	client := suite.client
+
+	var err error
+	var idResponse *piazza.AlerterIdResponse
+
 	var e1 piazza.Event
 	e1.Type = piazza.EventDataIngested
 	e1.Date = "22 Jan 2016"
 	e1.Data = nil
-	e1ID := postEvent(t, e1)
+	idResponse, err = client.PostToEvents(&e1)
+	assert.NoError(t, err)
+	e1ID := idResponse.ID
 	assert.Equal(t, "E1", e1ID)
 
 	var e2 piazza.Event
 	e2.Type = piazza.EventDataAccessed
 	e2.Date = "22 Jan 2016"
 	e2.Data = nil
-	e2ID := postEvent(t, e2)
+	idResponse, err = client.PostToEvents(&e2)
+	assert.NoError(t, err)
+	e2ID := idResponse.ID
 	assert.Equal(t, "E2", e2ID)
 
-	es := getEvents(t)
-	assert.Len(t, es, 2)
-	assert.Contains(t, es, "E1")
-	assert.Contains(t, es, "E2")
+	es, err := client.GetFromEvents()
+	assert.NoError(t, err)
+	assert.Len(t, *es, 2)
+	ok1 := false
+	ok2 := false
+	for k, _ := range *es {
+		if k == "E1" {
+			ok1 = true
+		}
+		if k == "E2" {
+			ok2 = true
+		}
+	}
+	assert.True(t, ok1 && ok2)
 }
 
 func (suite *AlerterTester) TestTriggering() {
 	t := suite.T()
 
-	cs := getConditions(t)
-	assert.Len(t, cs, 0)
+	client := suite.client
+
+	var err error
+	var idResponse *piazza.AlerterIdResponse
+
+	cs, err := client.GetFromConditions()
+	assert.NoError(t, err)
+	assert.Len(t, *cs, 0)
 
 	var rawC3 piazza.Condition
 	rawC3.Title = "cond1 title"
@@ -250,7 +165,9 @@ func (suite *AlerterTester) TestTriggering() {
 	rawC3.Type = piazza.EventDataIngested
 	rawC3.UserID = "user1"
 	rawC3.Date = time.Now().String()
-	c3ID := postCondition(t, rawC3)
+	idResponse, err = client.PostToConditions(&rawC3)
+	assert.NoError(t, err)
+	c3ID := idResponse.ID
 	assert.Equal(t, "3", c3ID)
 
 	var rawC4 piazza.Condition
@@ -259,7 +176,9 @@ func (suite *AlerterTester) TestTriggering() {
 	rawC4.Type = piazza.EventDataAccessed
 	rawC4.UserID = "user2"
 	rawC4.Date = time.Now().String()
-	c4ID := postCondition(t, rawC4)
+	idResponse, err = client.PostToConditions(&rawC4)
+	assert.NoError(t, err)
+	c4ID := idResponse.ID
 	assert.Equal(t, "4", c4ID)
 
 	var rawC5 piazza.Condition
@@ -268,51 +187,57 @@ func (suite *AlerterTester) TestTriggering() {
 	rawC5.Type = piazza.EventFoo
 	rawC5.UserID = "user2"
 	rawC5.Date = time.Now().String()
-	c5ID := postCondition(t, rawC5)
+	idResponse, err = client.PostToConditions(&rawC5)
+	assert.NoError(t, err)
+	c5ID := idResponse.ID
 	assert.Equal(t, "5", c5ID)
 
-	cs = getConditions(t)
-	assert.Len(t, cs, 3)
+	cs, err = client.GetFromConditions()
+	assert.NoError(t, err)
+	assert.Len(t, *cs, 3)
 
 	var e3 piazza.Event
 	e3.Type = piazza.EventDataAccessed
 	e3.Date = time.Now().String()
 	e3.Data = map[string]string{"file": "111.tif"}
-	e3ID := postEvent(t, e3)
+	idResponse, err = client.PostToEvents(&e3)
+	assert.NoError(t, err)
+	e3ID := idResponse.ID
 	assert.Equal(t, "E3", e3ID)
 
 	var e4 piazza.Event
 	e4.Type = piazza.EventDataIngested
 	e4.Date = time.Now().String()
 	e4.Data = map[string]string{"file": "111.tif"}
-	e4ID := postEvent(t, e4)
+	idResponse, err = client.PostToEvents(&e4)
+	assert.NoError(t, err)
+	e4ID := idResponse.ID
 	assert.Equal(t, "E4", e4ID)
 
 	var e5 piazza.Event
 	e5.Type = piazza.EventBar
 	e5.Date = time.Now().String()
-	e5ID := postEvent(t, e5)
+	idResponse, err = client.PostToEvents(&e5)
+	assert.NoError(t, err)
+	e5ID := idResponse.ID
 	assert.Equal(t, "E5", e5ID)
 
-	as := getAlerts(t)
-	assert.Len(t, as, 2)
+	as, err := client.GetFromAlerts()
+	assert.NoError(t, err)
+	assert.Len(t, *as, 2)
 
 	// TODO: dependent on order of returned results
-	if as[0].ID == "A1" {
-		assert.Equal(t, "A1", as[0].ID)
-		assert.Equal(t, "E3", as[0].EventID)
-		assert.Equal(t, "4", as[0].ConditionID)
-		assert.Equal(t, "A2", as[1].ID)
-		assert.Equal(t, "E4", as[1].EventID)
-		assert.Equal(t, "3", as[1].ConditionID)
-	} else {
-		assert.Equal(t, "A2", as[0].ID)
-		assert.Equal(t, "E4", as[0].EventID)
-		assert.Equal(t, "3", as[0].ConditionID)
-		assert.Equal(t, "A1", as[1].ID)
-		assert.Equal(t, "E3", as[1].EventID)
-		assert.Equal(t, "4", as[1].ConditionID)
-	}
+	v1, ok := (*as)["A1"]
+	assert.True(t, ok)
+	v2, ok := (*as)["A2"]
+	assert.True(t, ok)
+
+	assert.Equal(t, "A1", v1.ID)
+	assert.Equal(t, "E3", v1.EventID)
+	assert.Equal(t, "4", v1.ConditionID)
+	assert.Equal(t, "A2", v2.ID)
+	assert.Equal(t, "E4", v2.EventID)
+	assert.Equal(t, "3", v2.ConditionID)
 }
 
 func (suite *AlerterTester) TestAdmin() {
--- client.go
+++ client.go
@@ -10,7 +10,7 @@ import (
 	"io/ioutil"
 )
 
-type PzAlerterClient struct{
+type PzAlerterClient struct {
 	url string
 }
 
@@ -20,6 +20,177 @@ func NewPzAlerterClient(address string) *PzAlerterClient {
 
 	return c
 }
+
+func (c *PzAlerterClient) PostToEvents(event *piazza.Event) (*piazza.AlerterIdResponse, error) {
+	body, err := json.Marshal(event)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(c.url + "/events", piazza.ContentTypeJSON, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		return nil, errors.New(resp.Status)
+	}
+
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := new(piazza.AlerterIdResponse)
+	err = json.Unmarshal(data, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *PzAlerterClient) GetFromEvents() (*piazza.EventList, error) {
+	resp, err := http.Get(c.url + "/events")
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	d, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var x piazza.EventList
+	err = json.Unmarshal(d, &x)
+	if err != nil {
+		return nil, err
+	}
+
+	return &x, nil
+}
+
+func (c *PzAlerterClient) GetFromAlerts() (*piazza.AlertList, error) {
+	resp, err := http.Get(c.url + "/alerts")
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	d, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var x piazza.AlertList
+	err = json.Unmarshal(d, &x)
+	if err != nil {
+		return nil, err
+	}
+
+	return &x, nil
+}
+
+func (c *PzAlerterClient) PostToConditions(cond *piazza.Condition) (*piazza.AlerterIdResponse, error) {
+	body, err := json.Marshal(cond)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(c.url + "/conditions", piazza.ContentTypeJSON, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		return nil, errors.New(resp.Status)
+	}
+
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := new(piazza.AlerterIdResponse)
+	err = json.Unmarshal(data, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *PzAlerterClient) GetFromConditions() (*piazza.ConditionList, error) {
+	resp, err := http.Get(c.url + "/conditions")
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	d, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var x piazza.ConditionList
+	err = json.Unmarshal(d, &x)
+	if err != nil {
+		return nil, err
+	}
+
+	return &x, nil
+}
+
+func (c *PzAlerterClient) GetFromCondition(id string) (*piazza.Condition, error) {
+	resp, err := http.Get(c.url + "/conditions/" + id)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	d, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var x piazza.Condition
+	err = json.Unmarshal(d, &x)
+	if err != nil {
+		return nil, err
+	}
+
+	if id != x.ID {
+		return nil, errors.New("internal error, condition ID mismatch")
+	}
+
+	return &x, nil
+}
+
+func (c *PzAlerterClient) DeleteOfCondition(id string) error {
+	resp, err := piazza.Delete(c.url + "/conditions/" + id)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New(resp.Status)
+	}
+	return nil
+}
+
 
 func (c *PzAlerterClient) GetFromAdminStats() (*piazza.AlerterAdminStats, error) {
 
