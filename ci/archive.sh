7f8018ff02975bb1c610ba7d414364c1af3e0923
Seth Baxter <sethbaxter@prominentedge.com>
2016-03-16 17:30:46 -0400

    new ci files

---
+++ ci/archive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ex
+
+pushd `dirname $0`/.. > /dev/null
+root=$(pwd -P)
+popd > /dev/null
+
+export GOPATH=$root/gogo
+mkdir -p $GOPATH
+
+###
+
+go get github.com/venicegeo/pz-workflow
+
+go install github.com/venicegeo/pz-workflow/server
+go install github.com/venicegeo/pz-workflow/client
+
+###
+
+src=$GOPATH/bin/pz-workflow
+
+# gather some data about the repo
+source $root/ci/vars.sh
+
+# stage the artifact for a mvn deploy
+mv $src $root/$APP.$EXT
---
+++ ci/vars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+APP=pz-workflow
+EXT=bin
