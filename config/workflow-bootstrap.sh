f391565c4fea2f5d9acdb23bf05b14db4a9d52d5
Sonny Saniev <sonny.saniev@rgi-corp.com>
2016-07-11 14:51:21 -0400

    updates

--- config/workflow-bootstrap.sh
+++ config/workflow-bootstrap.sh
@@ -3,15 +3,15 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # install openjdk-7 
-sudo apt-get purge openjdk*
-sudo apt-get -y install openjdk-7-jdk
+#sudo apt-get purge openjdk*
+#sudo apt-get -y install openjdk-7-jdk
 
-#get golang
+# get golang
 cd /usr/local
 sudo wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
 
-#add go to path
+# add go to path
 echo 'export PATH=$PATH:/usr/local/go/bin' >>/home/vagrant/.bash_profile
 export PATH=$PATH:/usr/local/go/bin
 
@@ -20,23 +20,19 @@ go help
 
 apt-get -y install git
 
-#build and start the go app
-#echo Creating new workspace directory at home directory
+# build and start the go app
 mkdir /home/vagrant/workspace
 cd /home/vagrant/workspace
 
-#echo setting GOPATH...
+# setting some env variables
 export GOPATH=/home/vagrant/workspace/gostuff
-#echo 'export GOPATH=/home/vagrant/workspace/gostuff' >>/home/vagrant/.bash_profile
+export VCAP_SERVICES='{"user-provided":[{"credentials":{"host":"192.168.44.44:9200","hostname":"192.168.44.44","port":"9200"},"label":"user-provided","name":"pz-elasticsearch","syslog_drain_url":"","tags":[]}]}'
 
-#echo getting pz-uuidgen and trying to build it.....................
+# getting pz-workflow and trying to build it
 go get github.com/venicegeo/pz-workflow
 go install github.com/venicegeo/pz-workflow
 
 #start the app
 cd /home/vagrant/workspace/gostuff/bin
 
-echo List of workflow executables:
-ls -la
-
-./pz-workflow
+./pz-workflow PORT=5000
