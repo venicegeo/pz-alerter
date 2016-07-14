8cf9dd1c7524b88e6d90b3afa1b11c7fb995345a
mpg <mpg@flaxen.com>
2016-07-14 16:39:04 -0400

    rebuilding QueryParams support class

--- demo/test-eventtypes-order.sh
+++ demo/test-eventtypes-order.sh
@@ -5,8 +5,10 @@ url="http://pz-workflow.$PZDOMAIN"
 echo
 echo GET /event
 
-ret=$(curl -S -s -XGET "$url"/eventType?perPage=13&order=desc&page=1)
+#ret=$(curl -S -s -XGET "$url"/eventType?perPage=13&order=asc&page=1&sortBy=createdOn)
 
-echo "$ret" | grep createdOn
+ret=$(curl -S -s -XGET "$url"/eventType\?order=asc\&perPage=13\&sortBy=eventTypeId)
 
-echo "$ret"
+echo "$ret" | grep eventTypeId
+
+#echo "$ret"
