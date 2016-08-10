505abe88cb1a6fe5fac91353357b86390a7384e1
wkhunt <wkhunt3@gmail.com>
2016-08-10 08:52:10 -0400

    Trigger now checks serviceid exists

--- workflow/TriggerDB.go
+++ workflow/TriggerDB.go
@@ -17,6 +17,7 @@ package workflow
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/venicegeo/pz-gocommon/elasticsearch"
 	"github.com/venicegeo/pz-gocommon/gocommon"
@@ -38,6 +39,32 @@ func NewTriggerDB(service *WorkflowService, esi elasticsearch.IIndex) (*TriggerD
 }
 
 func (db *TriggerDB) PostTrigger(trigger *Trigger, id piazza.Ident) (piazza.Ident, error) {
+
+	{ //CHECK SERVICE EXISTS
+		jobData := trigger.Job.JobType.Data
+		serviceId := jobData["serviceId"]
+		strServiceId, ok := serviceId.(string)
+		if !ok {
+			return piazza.NoIdent, LoggedError("TriggerDB.PostData faile: serviceId field not of type string")
+		}
+		serviceControllerURL, err := db.service.sys.GetAddress("pz-servicecontroller")
+		if err != nil {
+			return piazza.NoIdent, LoggedError("TriggerDB.PostData failed to find ServiceController: %s", err)
+		}
+		serviceControllerURL = "http://" + serviceControllerURL + "/service/" + strServiceId
+		req, err := http.NewRequest("GET", serviceControllerURL, nil)
+		if err != nil {
+			return piazza.NoIdent, LoggedError("TriggerDB.PostData failed to make request to ServiceController: %s", err)
+		}
+		client := &http.Client{}
+		response, err := client.Do(req)
+		if err != nil {
+			return piazza.NoIdent, LoggedError("TriggerDB.PostData failed to reach ServiceController: %s", err)
+		}
+		if response.StatusCode != 200 {
+			return piazza.NoIdent, LoggedError("TriggerDB.PostData: serviceId %s does not exist", strServiceId)
+		}
+	}
 
 	ifaceObj := trigger.Condition.Query
 	//log.Printf("Query: %v", ifaceObj)
