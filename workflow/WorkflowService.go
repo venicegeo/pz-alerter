1eaf5df3594135dfd433519630192eb80ba15b96
John McMahon <jmcmahon@radiantblue.com>
2016-08-23 16:22:09 -0400

    Pass in eventTypeName

--- workflow/WorkflowService.go
+++ workflow/WorkflowService.go
@@ -422,7 +422,7 @@ func (service *WorkflowService) DeleteEventType(id piazza.Ident) *piazza.JsonRes
 	if hits > 0 || len(triggers) > 0 {
 		return service.statusForbidden(errors.New("Deleting eventTypes that are in use is prohibited"))
 	}
-	events, hits, err := service.eventDB.GetEventsByEventTypeId(id)
+	events, hits, err := service.eventDB.GetEventsByEventTypeId(eventType.Name, id)
 	if hits > 0 || len(events) > 0 {
 		return service.statusForbidden(errors.New("Deleting eventTypes that are in use is prohibited"))
 	}
