6ee0cb71f7ed23e210b4398fa8e0ceb8f353c037
John McMahon <jmcmahon@radiantblue.com>
2016-09-08 12:31:13 -0400

    Start adding tests for dsl queries

--- workflow/Client.go
+++ workflow/Client.go
@@ -167,6 +167,12 @@ func (c *Client) PostEventType(eventType *EventType) (*EventType, error) {
 	return out, err
 }
 
+func (c *Client) QueryEventTypes(query map[string]interface{}) (*[]EventType, error) {
+	out := &[]EventType{}
+	err := c.postObject(query, "/eventType/query", out)
+	return out, err
+}
+
 func (c *Client) PutEventType(eventType *EventType) (*EventType, error) {
 	out := &EventType{}
 	err := c.putObject(eventType, "/eventType", out)
@@ -204,6 +210,12 @@ func (c *Client) PostEvent(event *Event) (*Event, error) {
 	return out, err
 }
 
+func (c *Client) QueryEvents(query map[string]interface{}) (*[]Event, error) {
+	out := &[]Event{}
+	err := c.postObject(query, "/event/query", out)
+	return out, err
+}
+
 func (c *Client) PutEvent(event *Event) (*Event, error) {
 	out := &Event{}
 	err := c.putObject(event, "/event", out)
@@ -232,6 +244,12 @@ func (c *Client) GetAllTriggers() (*[]Trigger, error) {
 func (c *Client) PostTrigger(trigger *Trigger) (*Trigger, error) {
 	out := &Trigger{}
 	err := c.postObject(trigger, "/trigger", out)
+	return out, err
+}
+
+func (c *Client) QueryTriggers(query map[string]interface{}) (*[]Trigger, error) {
+	out := &[]Trigger{}
+	err := c.postObject(query, "/trigger/query", out)
 	return out, err
 }
 
@@ -269,6 +287,12 @@ func (c *Client) GetAllAlerts() (*[]Alert, error) {
 func (c *Client) PostAlert(alert *Alert) (*Alert, error) {
 	out := &Alert{}
 	err := c.postObject(alert, "/alert", out)
+	return out, err
+}
+
+func (c *Client) QueryAlerts(query map[string]interface{}) (*[]Alert, error) {
+	out := &[]Alert{}
+	err := c.postObject(query, "/alert/query", out)
 	return out, err
 }
 
