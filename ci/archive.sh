ce6d6f71834b390406033383e5f244325a13361e
mpg <mpg@flaxen.com>
2016-10-10 15:47:03 -0400

    add linter, save build artifacts

--- ci/archive.sh
+++ ci/archive.sh
@@ -4,30 +4,27 @@ set -e
 pushd "$(dirname "$0")/.." > /dev/null
 root=$(pwd -P)
 popd > /dev/null
-
-#----------------------------------------------------------------------
-
 export GOPATH=$root/gogo
-mkdir -p "$GOPATH"
-
-# glide expects these to already exist
-mkdir "$GOPATH"/bin "$GOPATH"/src "$GOPATH"/pkg
-
-PATH=$PATH:"$GOPATH"/bin
-
-curl https://glide.sh/get | sh
-
-# get ourself, and go there
-go get github.com/venicegeo/pz-workflow
-cd $GOPATH/src/github.com/venicegeo/pz-workflow
 
 #----------------------------------------------------------------------
 
-src=$GOPATH/bin/pz-workflow
+sh $root/ci/do_build.sh
+
+#----------------------------------------------------------------------
+
+app=$GOPATH/bin/pz-workflow
 
 # gather some data about the repo
-# shellcheck disable=SC1090
-source "$root/ci/vars.sh"
+source $root/ci/vars.sh
 
-# stage the artifact for a mvn deploy
-mv "$src" "$root"/"$APP"."$EXT"
+# stage the artifact(s) for a mvn deploy
+mv $app $root/$APP.$EXT
+
+cd $root
+tar cvzf $APP.tgz \
+    $APP.$EXT \
+    workflow.cov \
+    lint.txt \
+    glide.lock \
+    glide.yaml
+tar tzf $APP.tgz
--- ci/test.sh
+++ ci/test.sh
@@ -3,23 +3,8 @@
 pushd "$(dirname "$0")"/.. > /dev/null
 root=$(pwd -P)
 popd > /dev/null
-
-#----------------------------------------------------------------------
-
 export GOPATH=$root/gogo
-mkdir -p "$GOPATH"
-
-# glide expects these to already exist
-mkdir "$GOPATH"/bin "$GOPATH"/src "$GOPATH"/pkg
-
-PATH=$PATH:"$GOPATH"/bin
-
-curl https://glide.sh/get | sh
-
-# get ourself, and go there
-go get github.com/venicegeo/pz-workflow
-cd $GOPATH/src/github.com/venicegeo/pz-workflow
 
 #----------------------------------------------------------------------
 
-go test -v -coverprofile=workflow.cov github.com/venicegeo/pz-workflow/workflow
+sh $root/ci/do_build.sh
