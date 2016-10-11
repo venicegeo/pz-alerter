6a3c6692706101331f45681a532f2d5799047b5e
mpg <mpg@flaxen.com>
2016-10-11 12:20:50 -0400

    8818 - update deps

--- vendor/github.com/venicegeo/pz-uuidgen/ci/archive.sh
+++ vendor/github.com/venicegeo/pz-uuidgen/ci/archive.sh
@@ -3,29 +3,23 @@
 pushd `dirname $0`/.. > /dev/null
 root=$(pwd -P)
 popd > /dev/null
-
-#----------------------------------------------------------------------
-
 export GOPATH=$root/gogo
-mkdir -p "$GOPATH"
-
-# glide expects these to already exist
-mkdir "$GOPATH"/bin "$GOPATH"/src "$GOPATH"/pkg
-
-PATH=$PATH:"$GOPATH"/bin
-
-curl https://glide.sh/get | sh
-
-# get ourself, and go there
-go get github.com/venicegeo/pz-uuidgen
-cd $GOPATH/src/github.com/venicegeo/pz-uuidgen
 
 #----------------------------------------------------------------------
 
-src=$GOPATH/bin/pz-uuidgen
+sh $root/ci/do_build.sh
+
+#----------------------------------------------------------------------
 
 # gather some data about the repo
 source $root/ci/vars.sh
 
-# stage the artifact for a mvn deploy
-mv $src $root/$APP.$EXT
+cd $root
+cp $GOPATH/bin/$APP .
+tar cvzf $APP.$EXT \
+    $APP \
+    uuidgen.cov \
+    lint.txt \
+    glide.lock \
+    glide.yaml
+tar tzf $APP.$EXT
--- vendor/github.com/venicegeo/pz-uuidgen/ci/test.sh
+++ vendor/github.com/venicegeo/pz-uuidgen/ci/test.sh
@@ -3,25 +3,8 @@
 pushd `dirname $0`/.. > /dev/null
 root=$(pwd -P)
 popd > /dev/null
-
-#----------------------------------------------------------------------
-
 export GOPATH=$root/gogo
-mkdir -p "$GOPATH"
-
-# glide expects these to already exist
-mkdir "$GOPATH"/bin "$GOPATH"/src "$GOPATH"/pkg
-
-PATH=$PATH:"$GOPATH"/bin
-
-curl https://glide.sh/get | sh
-
-# get ourself, and go there
-go get github.com/venicegeo/pz-uuidgen
-cd $GOPATH/src/github.com/venicegeo/pz-uuidgen
 
 #----------------------------------------------------------------------
 
-go test -v -coverprofile=uuidgen.cov github.com/venicegeo/pz-uuidgen/uuidgen
-
-###
+sh $root/ci/do_build.sh
